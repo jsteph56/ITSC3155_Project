1004adb06a1a569ac9f14826b27a56a8990fdb5a
Emily Ruttenberg <eruttenb@uncc.edu>
2021-11-11 08:37:12 -0500

    Added button functionality

--- flask01.py
+++ flask01.py
@@ -2,16 +2,31 @@
 
 #Imports
 import os
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 
 #Create the app
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/questions')
+def questions():
+    return render_template('questions.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
