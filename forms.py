648436b0adefbc42b3b2cb2dd57d02b9d34f4359
Emily Ruttenberg <eruttenb@uncc.edu>
2021-11-16 08:49:26 -0500

    Added login, register, logout, comment features from flask assignments. Started implementing like feature.

---
+++ forms.py
@@ -0,0 +1,58 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import Length, Regexp, DataRequired, EqualTo, Email
+from wtforms import ValidationError
+from models import User
+from database import db
+
+
+class RegisterForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    name = StringField('Name', validators=[Length(1, 30)])
+
+    email = StringField('Email', [
+        Email(message='Not a valid email address.'),
+        DataRequired()])
+
+    password = PasswordField('Password', [
+        DataRequired(message="Please enter a password."),
+        EqualTo('confirmPassword', message='Passwords must match')
+    ])
+
+    confirmPassword = PasswordField('Confirm Password', validators=[
+        Length(min=8, max=20)
+    ])
+    submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if db.session.query(User).filter_by(email=field.data).count() != 0:
+            raise ValidationError('Username already in use.')
+
+
+class LoginForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    email = StringField('Email', [
+        Email(message='Not a valid email address.'),
+        DataRequired()])
+
+    password = PasswordField('Password', [
+        DataRequired(message="Please enter a password.")])
+
+    submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if db.session.query(User).filter_by(email=field.data).count() == 0:
+            raise ValidationError('Incorrect username or password.')
+
+
+class CommentForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    comment = TextAreaField('Comment', validators=[Length(min=1)])
+
+    submit = SubmitField('Add Comment')
--- models.py
+++ models.py
@@ -1,4 +1,12 @@
 from database import db
+import datetime
+
+class Like(db.Model):
+    __tablename__ = "like"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey("question.id"), nullable=False)
+    questions = db.relationship("Question", backref="like", lazy=True)
 
 
 class Question(db.Model):
@@ -10,8 +18,13 @@ class Question(db.Model):
     dislikes = db.Column("dislikes", db.Integer)
     topics = db.Column("topics", db.String(10))
     # imageURL = db.Column("imageURL", db.String(500)) - implement later
+    #Can create a foreign key
+    #Referencing the id variable in the User class, which is why it is a lowercase u
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship("Comment", backref="question", cascade="all, delete-orphan", lazy=True)
+    likes = db.relationship("Like", backref="question", lazy=True)
 
-    def __init__(self, header, body, date, topics):
+    def __init__(self, header, body, date, topics, user_id):
         self.header = header
         self.body = body
         self.date = date
@@ -19,15 +32,50 @@ class Question(db.Model):
         self.dislikes = 0
         self.topics = topics
         # self.imageURL = imageURL
+        self.user_id = user_id
 
 
 class User(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column("name", db.String(100))
     email = db.Column("email", db.String(100))
-    password = db.Column("password", db.String(50))
+    password = db.Column("password", db.String(50), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    questions = db.relationship("Question", backref="user", lazy=True)
+    comments = db.relationship("Comment", backref="user", lazy=True)
+    liked = db.relationship("Like", foreign_keys="Like.user_id", backref="user", lazy=True)
 
-    def __init(self, name, email, password):
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
+        self.registered_on = datetime.date.today()
+
+    #Liking post helper methods
+    def like_question(self, question):
+        if self.liked_question():
+            return
+        else:
+            new_like = Like(user_id=self.id, question_id=question.id)
+            db.session.add(new_like)
+
+    def unlike_question(self, question):
+        if self.liked_question(question):
+            Like.query.filter_by(user_id=self.id, question_id=question.id).delete()
+
+    def liked_question(self, question):
+        return Like.query.filter(Like.user_id == self.id, Like.question_id == question.id).count() > 0
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey("question.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, question_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.question_id = question_id
+        self.user_id = user_id
